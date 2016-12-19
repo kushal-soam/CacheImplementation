2bf111ee631f5071f32059ff837878cbceb82a81
kushal-soam <ksin72@sapient.com>
2016-12-19 11:20:33 +0530

    Initial Commit

---
+++ CacheImplementation/src/com/sapient/ace/Employee.java
@@ -0,0 +1,29 @@
+package com.sapient.ace;
+
+public class Employee
+{
+
+	private String name;
+	private Integer id;
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public Integer getId()
+	{
+		return id;
+	}
+
+	public void setId(Integer id)
+	{
+		this.id = id;
+	}
+
+}
